63bdcde4910f06d665b763348ca2327e23d21a80
miyako6105 <miyako.s6105@gmail.com>
2025-12-24 15:28:01 +0900

    データセットスクリプトの修正(qa-verify対応版)

--- inference/download_dataset.py
+++ inference/download_dataset.py
@@ -10,7 +10,7 @@ def main():
     parser.add_argument(
         "--dataset_name",
         type=str,
-        default="team-victory/qa_verify_10k_test",
+        default="HayatoHongoEveryonesAI/qa_verify_255k_v4",
         help="dataset name on Hugging Face Hub",
     )
     args = parser.parse_args()
@@ -18,14 +18,22 @@ def main():
     # データセットのダウンロード
     # load_datasetのchace_dirで保存先を指定
     model_path = Path("./datasets") / args.dataset_name.replace("/", "-")
-    dataset = load_dataset(args.dataset_name, cache_dir=str(model_path), split="test")
+    dataset = load_dataset(args.dataset_name, cache_dir=str(model_path))
+    # train test で分割
+    train_dataset = dataset["train"]
+    test_dataset = dataset["test"]
 
     print(f"Dataset downloaded to: {model_path}")
 
     # json形式で保存する
-    output_path = "./input/math500-ja.jsonl"
-    with open(output_path, "w", encoding="utf-8") as f:
-        for i, item in enumerate(dataset):
+    output_base = "./input/qa-verify-255k-v4"
+    with open(output_base + "-train.jsonl", "w", encoding="utf-8") as f:
+        for i, item in enumerate(train_dataset):
+            # idを振り直す
+            item["id"] = i
+            f.write(json.dumps(item, ensure_ascii=False) + "\n")
+    with open(output_base + "-test.jsonl", "w", encoding="utf-8") as f:
+        for i, item in enumerate(test_dataset):
             # idを振り直す
             item["id"] = i
             f.write(json.dumps(item, ensure_ascii=False) + "\n")
