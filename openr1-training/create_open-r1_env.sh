aeb6c98574cd23cdee96a3d93c063f3af5cea218
KotShinZ <s18574s18574@gmail.com>
2026-01-03 18:12:16 +0900

    update env command.

--- openr1-training/create_open-r1_env.sh
+++ openr1-training/create_open-r1_env.sh
@@ -6,14 +6,15 @@
 # module load python/3.12/3.12.9
 # module load cuda/12.8
 
-uv venv env # venvの作成
-source env/bin/activate # venvの有効化
+uv venv env2 # venvの作成
+source env2/bin/activate # venvの有効化
 uv pip install --upgrade pip # pipのアップグレード
 
 
 uv pip install trl[vllm]
-uv pip install setuptools && uv pip install flash-attn --no-build-isolation 
+uv pip install setuptools
 GIT_LFS_SKIP_SMUDGE=1 uv pip install -e "open-r1[dev]" 
+uv pip install flash-attn --no-build-isolation 
 uv pip install peft
 
 # vllmのインストール
